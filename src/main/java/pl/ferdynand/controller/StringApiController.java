efada4d2160cdecaf8c2d8369c269d64c4180fa6
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-10-15 17:24:56 +0200

    add ValidateString

--- src/main/java/pl/ferdynand/controller/StringApiController.java
+++ src/main/java/pl/ferdynand/controller/StringApiController.java
@@ -1,6 +1,17 @@
 package pl.ferdynand.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.ferdynand.ui.model.response.ValidateString;
 
+@RestController
+@RequestMapping("api")
 public class StringApiController {
+    @GetMapping(value = "/validate")
+    public StringBuilder paramInfo(@RequestParam(name ="param") String param){
+        return new StringBuilder(ValidateString.tellVariableType(param));
+    }
 
 }
---
+++ src/main/java/pl/ferdynand/ui/model/response/ValidateString.java
@@ -0,0 +1,61 @@
+package pl.ferdynand.ui.model.response;
+
+public class ValidateString {
+
+    public static String tellVariableType(String value) {
+        StringBuilder response = new StringBuilder();
+        if(value.length() == 0)
+            return  "Null";
+        ValidateString validate = new ValidateString();
+        if(validate.stoi(value))
+            return "Integer";
+        if(validate.stod(value))
+            return "Double";
+        if(validate.isBoolean(value))
+            return "Boolean";
+        if(value.matches("[A-Za-z0-9]+"))
+            response.append("Alphanumeric ");
+        else
+            response.append("Not Alphanumeric ");
+        if(validate.allUpperCase(value))
+            response.append("String Upper Case ");
+        if(validate.allLowerCase(value))
+            response.append("String Lower Case ");
+        return response.toString();
+    }
+
+    private boolean allUpperCase(String value) {
+        String tmp = String.copyValueOf(value.toCharArray());
+        return value.equals(tmp.toUpperCase());
+    }
+
+    private boolean allLowerCase(String value) {
+        String tmp = String.copyValueOf(value.toCharArray());
+        return value.equals(tmp.toLowerCase());
+    }
+
+    private boolean stoi(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    private boolean stod(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isBoolean(String str){
+        if(str.equalsIgnoreCase("true") || str.equalsIgnoreCase("false"))
+            return true;
+        return false;
+    }
+
+}
