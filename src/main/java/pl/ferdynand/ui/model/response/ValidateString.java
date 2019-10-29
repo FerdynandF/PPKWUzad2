3a826c84204b56230241f28929a446a0955fe3b9
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-10-29 16:12:05 +0100

    refactor ValidateString

--- src/main/java/pl/ferdynand/ui/model/response/ValidateString.java
+++ src/main/java/pl/ferdynand/ui/model/response/ValidateString.java
@@ -2,64 +2,35 @@ package pl.ferdynand.ui.model.response;
 
 public class ValidateString {
 
-    public static TextDetails buildTextDetails(String text) {
+    public static TextDetails buildTextDetails(String text){
+        if(text == null)
+            return new TextDetails();
         TextDetails textDetails = new TextDetails();
-        textDetails.setHasNumber(true);
-        textDetails.setHasSmallLetter(true);
+        textDetails.setHasCapitalLetter(ValidateString.hasCapitalLetter(text));
+        textDetails.setHasSmallLetter(ValidateString.hasSmallLetter(text));
+        textDetails.setHasNumber(ValidateString.hasNumber(text));
+        textDetails.setHasSpecialCharacter(ValidateString.hasSpecialCharacter(text));
         return textDetails;
     }
 
-    public static String tellVariableType(String value) {
-        StringBuilder response = new StringBuilder();
-        if (value.length() == 0)
-            return "Null";
-        ValidateString validate = new ValidateString();
-        if (validate.stoi(value))
-            return "Integer";
-        if (validate.stod(value))
-            return "Double";
-        if (validate.isBoolean(value))
-            return "Boolean";
-        if (value.matches("[A-Za-z0-9]+"))
-            response.append("Alphanumeric ");
-        else
-            response.append("Not Alphanumeric ");
-        if (validate.allUpperCase(value))
-            response.append("String Upper Case ");
-        if (validate.allLowerCase(value))
-            response.append("String Lower Case ");
-        return response.toString();
+    private static boolean hasCapitalLetter(String text) {
+        boolean hasUpper = text.chars().anyMatch(Character::isUpperCase);
+        return hasUpper;
     }
 
-    private boolean allUpperCase(String value) {
-        String tmp = String.copyValueOf(value.toCharArray());
-        return value.equals(tmp.toUpperCase());
+    private static boolean hasSmallLetter(String text) {
+        boolean hasSmall = text.chars().anyMatch(Character::isLowerCase);
+        return hasSmall;
     }
 
-    private boolean allLowerCase(String value) {
-        String tmp = String.copyValueOf(value.toCharArray());
-        return value.equals(tmp.toLowerCase());
+    private static boolean hasNumber(String text) {
+        boolean hasNumber = text.chars().anyMatch(Character::isDigit);
+        return hasNumber;
     }
 
-    private boolean stoi(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    private boolean stod(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    private boolean isBoolean(String str) {
-        return str.equalsIgnoreCase("true") || str.equalsIgnoreCase("false");
+    private static boolean hasSpecialCharacter(String text) {
+        long hasSpecial =  text.chars().filter(value -> !Character.isDigit(value) && !Character.isLetter(value)).count();
+        return hasSpecial > 0;
     }
 }
+
